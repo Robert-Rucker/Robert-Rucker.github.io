07b7c4b16799ee34baf889ef3e77c33fb5a57aa7
Robert-Rucker <78342361+Robert-Rucker@users.noreply.github.com>
2021-06-02 15:44:31 -0500

    Delete 2021powwow.js

--- 2021powwow.js
+++
@@ -1,8 +0,0 @@
-var pointInfoJson = {
-"type": "FeatureCollection",
-"name": "2021powwow",
-"features": [
-{ "type": "Feature", "properties": { "city": "Little Eagle, SD", "longitude": -100.7949258, "latitude": 45.71554972, "reservation": "Standing Rock", "date": " 7\/9\/2021", "personOfIntrest": "Grand River" }, "geometry": null },
-{ "type": "Feature", "properties": { "city": null, "longitude": -101.1602212, "latitude": 45.75389099, "reservation": "Standing Rock", "date": "7\/9\/2021", "personOfIntrest": "Mr. Frank Jamerson" }, "geometry": null }
-]
-}
