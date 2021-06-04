820dcffde3ae5fbc9b7da6036f642e84ced2ffa3
Robert-Rucker <78342361+Robert-Rucker@users.noreply.github.com>
2021-06-03 19:33:24 -0500

    Update powwow2021.js

--- powwow2021.js
+++ powwow2021.js
@@ -2,7 +2,7 @@ var pointInfoJson = {
 "type": "FeatureCollection",
 "name": "2021powwow",
 "features": [
-{ "type": "Feature", "properties": { "city": "Little Eagle, SD", "longitude": -100.7949258, "latitude": 45.71554972, "reservation": "Standing Rock", "date": " 7\/9\/2021", "personOfIntrest": "Grand River" }, "geometry": { "type": "Point", "coordinates": [ -100.7949258, 45.71554972 ] } },
-{ "type": "Feature", "properties": { "city": null, "longitude": -101.1602212, "latitude": 45.75389099, "reservation": "Standing Rock", "date": "7\/9\/2021", "personOfIntrest": "Mr. Frank Jamerson" }, "geometry": { "type": "Point", "coordinates": [ -101.1602212, 45.75389099 ] } }
+{ "type": "Feature", "properties": { "city": "Little Eagle, SD", "longitude": -100.7949258, "latitude": 45.71554972, "reservation": "Standing Rock", "date": " 7\/9\/2021", "personOfIntrest": "Grand River, drum group.  This group has been singing for over 15 years." }, "geometry": { "type": "Point", "coordinates": [ -100.7949258, 45.71554972 ] } },
+{ "type": "Feature", "properties": { "city": null, "longitude": -101.1602212, "latitude": 45.75389099, "reservation": "Standing Rock", "date": "7\/9\/2021", "personOfIntrest": "Mr. Frank Jamerson, the Master of Ceremony for the 2021 North Platte Powwow, is from the Standing Rock Lakota Tribe." }, "geometry": { "type": "Point", "coordinates": [ -101.1602212, 45.75389099 ] } }
 ]
 }
